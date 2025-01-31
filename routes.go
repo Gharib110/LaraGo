315eee1fa3d03509234308ef2b4433a28b6d2444
Alireza Gharib <alirezagharib110@gmail.com>
2025-01-31 19:25:25 +0330

    Jet template is added to the routes.go

--- routes.go
+++ routes.go
@@ -9,6 +9,15 @@ func (a *application) routes() *chi.Mux {
 
 	a.App.Routes.Get("/", a.Handlers.Home)
 
+	a.App.Routes.Get("/jet", func(w http.ResponseWriter, r *http.Request) {
+		err := a.App.Render.JetPage(w, r, "home.jet", nil, nil)
+		if err != nil {
+			a.App.ErrorLog.Println("error rendering: ", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Error rendering 502"))
+			return
+		}
+	})
 	fileServer := http.FileServer(http.Dir("./public"))
 	a.App.Routes.Handle("/public/*", http.StripPrefix("/public", fileServer))
 	return a.App.Routes
